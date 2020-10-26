3ca9017f35e22d48176b73c89673ca497a407004
Edede Oiwoh <eoiwoh@gmail.com>
2020-10-26 16:52:09 -0400

    move db-wrapper to its own class

---
+++ src/services/data-svc/database-wrapper.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+export default class DatabaseWrapper {
+  db = firebase.firestore();
+
+  cache = {
+    experience: null,
+    education: null,
+    skills: null
+  };
+
+  /**
+   * Retrieve data from Firestore and add it to a cache.
+   * If data is present in the cache, cached data will be used.
+   * 
+   * @param collectionName The Firestore collection name
+   * @param useCache Optionally disable caching
+   */
+  async retrieveFromFirestore(collectionName, useCache = true) {
+    if (useCache && this.cache[collectionName]) {
+      return this.cache[collectionName];
+    } else {
+      const data = (await this.db.collection(collectionName)
+        .where('visible', '==', true)
+        .get()).docs.map(doc => doc.data());
+      this.cache[collectionName] = data;
+      return data;
+    }
+  }
+}
--- src/services/data-svc/index.js
+++ src/services/data-svc/index.js
@@ -1,38 +1,8 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
 import ExperienceData from './experience-data';
 import SkillsData from './skills-data';
 import EducationData from './education-data';
 import AppIcons from './icons';
-
-class DatabaseWrapper {
-  db = firebase.firestore();
-
-  cache = {
-    experience: null,
-    education: null,
-    skills: null
-  };
-
-  /**
-   * Retrieve data from Firestore and add it to a cache.
-   * If data is present in the cache, cached data will be used.
-   * 
-   * @param collectionName The Firestore collection name
-   * @param useCache Optionally disable caching
-   */
-  async retrieveFromFirestore(collectionName, useCache = true) {
-    if (useCache && this.cache[collectionName]) {
-      return this.cache[collectionName];
-    } else {
-      const data = (await this.db.collection(collectionName)
-        .where('visible', '==', true)
-        .get()).docs.map(doc => doc.data());
-      this.cache[collectionName] = data;
-      return data;
-    }
-  }
-}
+import DatabaseWrapper from './database-wrapper';
 
 export default class DataSvc {
   db = new DatabaseWrapper();
